d3828cb22f498c4f82c3d8d1aff4eefd6d4f6d1b
Dariem Perez <d.perez@dessa.com>
2019-01-08 10:57:37 -0500

    Use different channels for each request and context managers to automatically close them.

--- calculations_client.py
+++ calculations_client.py
@@ -3,31 +3,25 @@ import calculations_pb2_grpc
 import calculations_pb2
 
 
-def connect():
-    channel = grpc.insecure_channel('localhost:50051')
-    return channel
-
-
-channel = connect()
-
-
 def Area(shape_name, width, length):
-    stub = calculations_pb2_grpc.CalculationsServiceStub(channel)
-    response = stub.Area(calculations_pb2.CalculationsRequest(
-        shape_name=shape_name,
-        width=width,
-        length=length))
-    print(response.message, ': ', response.calculated_value)
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = calculations_pb2_grpc.CalculationsServiceStub(channel)
+        response = stub.Area(calculations_pb2.CalculationsRequest(
+            shape_name=shape_name,
+            width=width,
+            length=length))
+        print(response.message, ': ', response.calculated_value)
 
 
 def Volume(shape_name, width, length, height):
-    stub = calculations_pb2_grpc.CalculationsServiceStub(channel)
-    response = stub.Volume(calculations_pb2.CalculationsRequest(
-        shape_name=shape_name,
-        width=width,
-        length=length,
-        height=height))
-    print(response.message, ': ', response.calculated_value)
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = calculations_pb2_grpc.CalculationsServiceStub(channel)
+        response = stub.Volume(calculations_pb2.CalculationsRequest(
+            shape_name=shape_name,
+            width=width,
+            length=length,
+            height=height))
+        print(response.message, ': ', response.calculated_value)
 
 
 def main():
